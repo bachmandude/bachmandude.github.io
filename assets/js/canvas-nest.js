d7d4c5877b1a795367f8b3cfb9c3b1bd3fd219dd
bachmandude <117601487+bachmandude@users.noreply.github.com>
2023-12-12 19:45:17 -0600

    Update canvas-nest.js

--- assets/js/canvas-nest.js
+++ assets/js/canvas-nest.js
@@ -17,7 +17,7 @@
 			z: get_attribute(script, "zIndex", -1), //z-index
 			o: get_attribute(script, "opacity", .5), //opacity
 			c: get_attribute(script, "color", "0,0,0"), //color
-			n: get_attribute(script, "count", 99) //count
+			n: get_attribute(script, "count", 300) //count
 		}
 	}
 	//è®¾ç½®canvasçš„é«˜å®½
